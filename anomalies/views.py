97eb157d5156324125a923f7fa6dcf1ed9f4801a
pratik-1 <nandekarpratik04@gmail.com>
2022-07-09 19:16:40 +0100

    chenge permissions

--- anomalies/views.py
+++ anomalies/views.py
@@ -6,12 +6,12 @@ from anomalies.helper_functions import get_count_anomalies
 
 class CountAnomalies(APIView):
     """
-    View for authenticated users to find number of anomalies
+    View for authenticated users to find count of anomalies
     """
     def get(self, request, sensor_id=None, format=None):
         print(sensor_id)
         if sensor_id is None:
-            return Response('Provide the sensor_id in query parmeter (For ex: /count-anomalies/cr123/)')
+            return Response('Please provide the SensorId in query parameter (For ex: /count-anomalies/cr123/)')
         elif sensor_id:
             count = get_count_anomalies(sensor_id)
             return Response(f'The count of anomalies for sensor_id {sensor_id} is {count}')
